306895517682286d5a981a770957c82ddb2a326f
Yulypso <khamphousone.thierry@outlook.fr>
2021-05-03 11:10:38 +0200

    validate certificate + namespace + ClusterRole

--- generate-K8S-objects.sh
+++ generate-K8S-objects.sh
@@ -6,19 +6,18 @@ echo "Generate Kubernetes objects ..."
 # Create Namespaces
 #kubectl create namespace webhookserver-namespace # contains WebhookServer
 #kubectl create namespace admissionwebhook-namespace # to test deployments
-kubectl apply -f "WebhookDeployments/namespaces.yaml" # contains webhookserver-namespace and admissionwebhook-namespace
+kubectl apply -f "WebhookDeployments/namespaces.yml" # contains webhookserver-namespace and admissionwebhook-namespace
 
 # Create Secrets to store TLS certificate
-kubectl -n webhookserver-namespace create secret tls webhookserver-tls --key "Certificates/webhookservertls.key" --cert "Certificates/webhookservertls.cert"
+kubectl -n webhookserver-ns create secret tls webhookserver-tls --key "Certificates/webhookservertls.key" --cert "Certificates/webhookservertls.cert"
 
-# TEST: default namespace
-kubectl create secret tls webhookserver-tls --key "Certificates/webhookservertls.key" --cert "Certificates/webhookservertls.cert"
-
+# Create Webhookserver-deployment/yml objects
+kubectl apply -f "WebhookDeployments/webhookserver-deployment.yml"
 
 # Get CA_B64 flag replaced by ca.cert value within webhook-deployment.yaml 
-# Then create K8S webhook-deployment.yaml objects  
-ca_cert_B64=`openssl base64 -A < "Certificates/ca.cert"`
-sed -e 's@''${CA_B64}@'"$ca_cert_B64"'@g' < "WebhookDeployments/webhook-deployment.yaml" \
-    | kubectl create -f -
+# Then create K8S webhookconfiguration-deployment.yml object  
+#ca_cert_B64=`openssl base64 -A < "Certificates/ca.cert"`
+#sed -e 's@''${CA_B64}@'"$ca_cert_B64"'@g' < "WebhookDeployments/webhookconfiguration-deployment.yml" \
+#    | kubectl apply -f -
 
 echo "WebhookServer has been deployed..."
--- reset.sh
+++ reset.sh
@@ -3,8 +3,9 @@
 echo "Reset Kubernetes Cluster..."
 
 echo "Deleting Kubernetes Objects..."
-kubectl delete -f "WebhookDeployments/webhook-deployment.yaml" # delete webhook deployment
-kubectl delete -f "WebhookDeployments/namespaces.yaml" # delete namespaces + secrets contained
+kubectl delete -f "WebhookDeployments/webhookserver-deployment.yml" # delete webhook deployment
+#kubectl delete -f "WebhookDeployments/webhookconfiguration-deployment.yml"
+kubectl delete -f "WebhookDeployments/namespaces.yml" # delete namespaces + secrets contained
 
 
 if [ $"(tr "[:upper:]" "[:lower:]" <<< "$1")" == "certificates" ] || [ $"(tr "[:upper:]" "[:lower:]" <<< "$1")" == "certificate" ]; then
